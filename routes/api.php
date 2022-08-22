b657474e02fc7b9da6b83867f8901e745402e0e6
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-08-22 15:17:16 -0300

    FEAT: final modulo 3, listar modulos dos cursos.

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\Api\{
     CourseController,
+    ModuleController,
 };
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{id}', [CourseController::class, 'show']);
+
+Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
