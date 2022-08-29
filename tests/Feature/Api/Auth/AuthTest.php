a6c1c271f9a5e21890f97802e1fceb2c6a8c5971
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-08-29 17:56:48 -0300

    FEAT: Final aula 3 modulo 8, criando trait para retornar o token do usuario autenticado.

--- tests/Feature/Api/Auth/AuthTest.php
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -5,10 +5,13 @@ namespace Tests\Feature\Api\Auth;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Api\UtilsTrait;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
+    use UtilsTrait;
+
     public function test_fail_auth()
     {
         $response = $this->postJson('/auth', []);
@@ -36,8 +39,7 @@ class AuthTest extends TestCase
 
     public function test_logout()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('teste')->plainTextToken;
+        $token = $this->createTokenUser();
 
         $response = $this->postJson('/logout', [], [
             'Authorization' => "Bearer {$token}"
@@ -55,8 +57,7 @@ class AuthTest extends TestCase
 
     public function test_get_me()
     {
-        $user = User::factory()->create();
-        $token = $user->createToken('teste')->plainTextToken;
+        $token = $this->createTokenUser();
 
         $response = $this->getJson('/me', [
             'Authorization' => "Bearer {$token}"
