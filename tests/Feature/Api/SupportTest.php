3328b27f993d3b6a3282f0af59a9d8a303322a71
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-08-31 10:25:33 -0300

    FEAT: Final aula 9 modulo 9, Adiionando testes de lissons.

--- tests/Feature/Api/SupportTest.php
+++ tests/Feature/Api/SupportTest.php
@@ -34,4 +34,38 @@ class SupportTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonCount(50, 'data');
     }
+
+    public function test_get_supports_unauthenticated()
+    {
+        $response = $this->getJson('/supports');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_supports()
+    {
+        Support::factory()->count(50)->create();
+
+        $response = $this->getJson('/supports', $this->defaultHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_filter_lesson()
+    {
+        $lesson = Support::factory()->create();
+
+        Support::factory()->count(50)->create();
+        Support::factory()->count(10)->create([
+            'lesson_id' => $lesson->id
+        ]);
+
+        $payload = ['lesson' => $lesson->id];
+
+        $response = $this->json('GET', '/supports', $payload, $this->defaultHeaders());
+
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data');
+    }
 }
